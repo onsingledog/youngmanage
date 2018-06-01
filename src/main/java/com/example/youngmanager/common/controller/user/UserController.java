59a150958ea27724033f3b7e298b39471842c731
onesingledog <15232126623@163.com>
2018-06-01 18:11:15 +0800

    文件位置修改

--- src/main/java/com/example/youngmanager/common/controller/user/UserController.java
+++ src/main/java/com/example/youngmanager/common/controller/user/UserController.java
@@ -1,4 +1,4 @@
-package com.example.youngmanager.common.controller;
+package com.example.youngmanager.common.controller.user;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
